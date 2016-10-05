f408b2fafe9f4518c4eed622c9c4b47a2db46d64
Shriphani Palakodety <shriphanip@gmail.com>
2016-10-05 01:07:15 -0700

    three functions bro

--- src/clj_named_leveldb/core.clj
+++ src/clj_named_leveldb/core.clj
@@ -1,6 +1,36 @@
-(ns clj-named-leveldb.core)
+(ns clj-named-leveldb.core
+  (:refer-clojure :exclude [get sync])
+  (:require [clj-leveldb :as leveldb]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord NamedDB [db name])
+
+(defn make-named-db
+  "Args:
+    db: a level-db
+    name: a db-name used as a key prefix"
+  ([db]
+   (make-named-db db ""))
+  ([db name]
+   (->NamedDB db name)))
+
+(defn get
+  [named-db k]
+  (let [db    (:db named-db)
+        name  (:name named-db)]
+    (leveldb/get db
+                 (str name k))))
+
+(defn put
+  [named-db k v]
+  (let [db    (:db named-db)
+        name  (:name named-db)]
+    (leveldb/put db
+                 (str name k)
+                 v)))
+
+(defn delete
+  [named-db k v]
+  (let [db    (:db named-db)
+        name  (:name named-db)]
+    (leveldb/delete db
+                    (str name k))))
