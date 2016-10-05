d98193db11965a26a0f3dff9864c0823d52cc483
Shriphani Palakodety <shriphanip@gmail.com>
2016-10-04 21:03:20 -0700

    initial commit

---
+++ test/clj_named_leveldb/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-named-leveldb.core-test
+  (:require [clojure.test :refer :all]
+            [clj-named-leveldb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
