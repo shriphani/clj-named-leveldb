77c0959517bda204003cabc2458de7687c4e3b80
Shriphani Palakodety <shriphanip@gmail.com>
2016-10-06 00:29:06 -0700

    added some tests ^.^

--- test/clj_named_leveldb/core_test.clj
+++ test/clj_named_leveldb/core_test.clj
@@ -1,7 +1,27 @@
 (ns clj-named-leveldb.core-test
   (:require [clojure.test :refer :all]
-            [clj-named-leveldb.core :refer :all]))
+            [clj-named-leveldb.core :refer :all]
+            [clj-leveldb :as leveldb]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-correctness
+  (testing "Create 2 named databases and check if shit works"
+    (let [db (leveldb/create-db "/tmp"
+                                {:key-decoder byte-streams/to-string 
+                                 :val-decoder byte-streams/to-string})
+
+          alpha-db (make-named-db db "alpha")
+          beta-db  (make-named-db db "beta")]
+
+      (put alpha-db (str 1) (str 1))
+
+      (put beta-db (str 2) (str 2))
+      
+      (is
+       (= "1"
+          (leveldb/get (:db alpha-db)
+                       "alpha1")))
+
+      (is
+       (= "2"
+          (leveldb/get (:db beta-db)
+                       "beta2"))))))
