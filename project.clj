e54a12e3f7edc4005cfe1c17153fe4126a6db4be
Shriphani Palakodety <shriphanip@gmail.com>
2016-10-06 00:50:02 -0700

    version #s.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-named-leveldb "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-named-leveldb "0.1.0"
+  :description "A hack to get named databases in leveldb"
+  :url "https://github.com/shriphani/clj-named-leveldb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
